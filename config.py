da95cd749dc7ecfe0a3046f52ea93aeeb58042be
Oleh-creator24 <KalimbetOleh@gmail.com>
2025-07-31 01:12:18 +0200

    Add config.py with environment configuration

---
+++ config.py
@@ -0,0 +1,20 @@
+
+
+from dotenv import load_dotenv
+import os
+
+# Загружаем переменные из .env
+load_dotenv()
+
+# MySQL
+MYSQL_CONFIG = {
+    'host': os.getenv('MYSQL_HOST'),
+    'user': os.getenv('MYSQL_USER'),
+    'password': os.getenv('MYSQL_PASSWORD'),
+    'database': os.getenv('MYSQL_DATABASE')
+}
+
+# MongoDB
+MONGO_URI = os.getenv('MONGO_URI')
+MONGO_DB_NAME = os.getenv('MONGO_DB_NAME')
+MONGO_COLLECTION_NAME = os.getenv('MONGO_COLLECTION_NAME')
